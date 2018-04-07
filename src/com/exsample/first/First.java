be2ab6a7509e9a69e2ea0cea29d6e77a9a8a5987
RomanS18 <37449383+RomanS18@users.noreply.github.com>
2018-04-07 10:45:13 +0300

    first commit

---
+++ src/com/exsample/first/First.java
@@ -0,0 +1,13 @@
+package com.exsample.first;
+
+public class First {
+    public static void main (String[] args)
+    {
+        if (5>4)
+        {
+            System.out.println("Good!");
+        }
+
+    }
+
+}
